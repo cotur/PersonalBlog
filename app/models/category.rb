55c84dfe30b90f0513aec82c0eec016d3bd8f762
Ahmet Çotur <ahmet.cotur@hotmail.com>
2019-05-26 22:24:26 +0300

    validations added

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+    validates_presence_of :title
+    
     has_many :post, :dependent => :destroy
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+  
+  validates_presence_of :title
+  validates_presence_of :content
+  validates_presence_of :category
+
   belongs_to :category
   belongs_to :user
 end
