8859c817a00d7af1b8b53d80c8f5ba20ed122c1d
Ahmet Çotur <ahmet.cotur@hotmail.com>
2019-05-28 03:34:07 +0300

    rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   
-
-  get 'authors/', to: "author#index", as:"authors"
+  get 'authors/', to: "author#index", as: "authors"
   get 'authors/:id', to: "author#show"
   
   get 'page/home'
   get 'page/keyword'
-  
+  get 'keyword/', to: "page#keyword"
+  get 'home', to: "page#home", as: "home"
+
   resources :posts
   resources :categories
   devise_for :users
   
   root to: "page#home"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
